6e2a1629780e66dfe58d97ac80b31e173bf963ab
“prateektiwari7” <prateek.tiwari456@gmail.com>
2019-09-06 16:14:15 +0530

    StateChangefromTx

---
+++ workflows/src/main/kotlin/com/template/flows/NameFlow.kt
@@ -0,0 +1,55 @@
+package com.template.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import com.template.contracts.NameContract
+import com.template.states.NameState
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.contracts.requireThat
+import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+
+@InitiatingFlow
+@StartableByRPC
+class NameFlow constructor( var name:String,
+                            var lastname : String, var party: Party): FlowLogic<SignedTransaction>() {
+
+    @Suspendable
+    override fun call() : SignedTransaction {
+
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val command = Command(NameContract.Commands.Create(), listOf(party).map { it.owningKey })
+
+        val namestate = NameState(name,lastname,party, UniqueIdentifier())
+
+        val txBuilder = TransactionBuilder(notary)
+                .addOutputState(namestate, NameContract.ID)
+                .addCommand(command)
+
+        txBuilder.verify(serviceHub)
+        val tx = serviceHub.signInitialTransaction(txBuilder)
+
+        val sessions = (namestate.participants - ourIdentity).map { initiateFlow(it as Party) }
+        val stx = subFlow(CollectSignaturesFlow(tx, sessions))
+        return subFlow(FinalityFlow(stx, sessions))
+    }
+
+}
+
+@InitiatedBy(NameFlow::class)
+class NameResponder(val counterpartySession: FlowSession) : FlowLogic<SignedTransaction>() {
+    @Suspendable
+    override fun call() : SignedTransaction {
+        val signedTransactionFlow = object : SignTransactionFlow(counterpartySession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output = stx.tx.outputs.single().data
+
+            }
+        }
+        val txWeJustSignedId = subFlow(signedTransactionFlow)
+        return subFlow(ReceiveFinalityFlow(counterpartySession, txWeJustSignedId.id))
+    }
+}
